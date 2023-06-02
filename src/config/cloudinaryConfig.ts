f6cd819b830b3214a4e724083e661adc7a702f76
GeraldOpitz <geraldopitz@gmail.com>
2023-06-02 15:00:40 -0400

    version funcional con TypeScript

---
+++ src/config/cloudinaryConfig.ts
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+    cloud_name: "dwv9duaob",
+    api_key: "483248633449788",
+    api_secret: "Ghp1xvmZXXGLC9HoFWojyCRAiUA"
+});
+
+module.exports = cloudinary;
